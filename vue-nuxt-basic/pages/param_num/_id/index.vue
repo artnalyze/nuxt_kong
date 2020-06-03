2c519541581946eed6effde647b34fae6ac1722f
Artnalyze <artnalyze@gmail.com>
2020-06-03 15:55:41 +0700

    Part A02 : validate parameter url string and number

---
+++ vue-nuxt-basic/pages/param_num/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome : {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  validate(data) {
+    return /^\d+$/.test(data.params.id)
+    // validate url only number
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ vue-nuxt-basic/pages/param_str/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome : {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  validate(data) {
+    return /^[A-Za-z]+$/.test(data.params.id)
+    // validate url only string
+  }
+}
+</script>
+
+<style scoped></style>
